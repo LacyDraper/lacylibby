b9d49869eb040e2f2f66f90816dd39557da22eb0
Libby L. Gerber <libby@libbygerber.com>
2021-08-02 12:35:02 -0700

    one static marker

--- src/App.js
+++ src/App.js
@@ -48,12 +48,16 @@ const center = {
 
 const options = {
     styles: mapStyles,
-    disableDefaultUI: true, //gets rid of all teh controls on the map, then add back in 
+    disableDefaultUI: true,  
     zoomControl: true,
 }
 
 const onLoad = marker => {
     console.log('marker: ', marker)
+}
+const position = {
+    lat: 47.6050,
+    lng: -122.3344
 }
 
 const App = () => {
@@ -61,16 +65,15 @@ const App = () => {
     
     const {isLoaded, loadError} = useLoadScript({
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-        //leaving out places library becuase not using the library 
-
+        
     });
 
     // we need to create a variable that holds the state of libraries component here so we can render the markers to the page
     
-   
+
     
-    const [markers, setMarkers] = React.useState([]);
-    const [selected, setSelected] = React.useState(null);
+    // const [markers, setMarkers] = React.useState([]);
+    // const [selected, setSelected] = React.useState(null);
 
     // const onMapClick = React.useCallback((event) => {
     //   setMarkers(current => [ //when user clicks, call the setMarkers function. Is that built in??
@@ -95,28 +98,29 @@ const App = () => {
         zoom = {15} 
         center = {center}
         options = { options }
-       
-       >
-        {libraryData.map((marker => (
+        id= "marker-example"
+
+        >
+        {/* {libraryData.map((marker => ( */}
         <Marker 
-            key={marker.id}     
-            //onLoad={onLoad}
+            // key={marker.id}     
+            onLoad={onLoad}
             
-            position = {{lat : marker.lat,lng: marker.lng}}
-            
+            // position = {{lat : marker.lat,lng: marker.lng}}
+            position = { position }
             icon= {{
             url: '/3redbooks.svg',
             scaledSize: new window.google.maps.Size(30,30),
             origin: new window.google.maps.Point(0,0),
             anchor: new window.google.maps.Point(15,15) 
             }}
-          onClick={() => {
-          setSelected(marker);
-          }}
+            // onClick={() => {
+            // setSelected(marker);
+            // }}
             />
-        )))}
+        {/* )))} */}
         
-        {selected ? (
+        {/* {selected ? (
         <InfoWindow position= {{lat: selected.lat, lng: selected.lng}} onCloseClick = {() => {
         setSelected(null); //have to reset to null once x is clicked on window so that they can pop up agian when clicked
         }}>
@@ -124,11 +128,10 @@ const App = () => {
             <h2> image goes here </h2>
             <p>Library inventory last updated {formatRelative(selected.time, new Date())}</p>
         </div>
-        </InfoWindow>) : null} 
+        </InfoWindow>) : null}  */}
         </GoogleMap>
         </div>;
     };
 
 
 export default App;
-  
